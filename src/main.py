7755a4a1f0857d26093cdd4c94eed965335297e0
Stanislav Bezzhanov <sbezzhanov@yandex.ru>
2025-05-22 19:30:02 +0300

    del debug messages

--- src/main.py
+++ src/main.py
@@ -15,34 +15,25 @@ def main():
         pdf_path = sys.argv[1]
         if os.path.isfile(pdf_path):
             extracted_text = service.process_pdf(pdf_path)
-            print("Извлечённый текст:")
-            print(extracted_text)
-            # Определяем язык
             try:
                 language = detect(extracted_text)
-                print(f"Обнаруженный язык: {language}")
             except LangDetectException:
                 language = None
-                print("Не удалось определить язык.")
 
-            # Если текст слишком короткий или не определён язык, пробуем OCR с разными языками
             if not extracted_text.strip() or not language:
                 processor = PDFProcessor()
                 text = processor.extract_text(pdf_path)  # lang='rus+eng' по умолчанию
                 try:
                     language = detect(text)
-                    print(f"OCR (rus+eng) обнаруженный язык: {language}")
                 except LangDetectException:
                     language = None
 
             txt_path = os.path.splitext(pdf_path)[0] + ".txt"
             with open(txt_path, "w", encoding="utf-8") as f:
                 f.write(extracted_text)
-            print(f"Текст сохранён в: {txt_path}")
         else:
-            print(f"Файл не найден: {pdf_path}")
-        service.stop()
-        return
+            service.stop()
+            return
 
     try:
         while True:
--- src/service.py
+++ src/service.py
@@ -6,15 +6,12 @@ class PDFTextExtractorService:
 
     def start(self):
         self.running = True
-        print("Service started. Listening for PDF files...")
 
     def stop(self):
         self.running = False
-        print("Service stopped.")
 
     def process_pdf(self, pdf_file_path):
         if not self.running:
-            print("Service is not running. Cannot process PDF.")
             return None
 
         processor = PDFProcessor()
